171020ae32ebcc78ebca9275ff3ce4cd1173242b
domHenin <domhenin.517@gmail.com>
2019-10-05 15:10:58 -0400

    initial setup

---
+++ src/io/migenjutsu/projects/build_email_administration_application/App.java
@@ -0,0 +1,7 @@
+package io.migenjutsu.projects.build_email_administration_application;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
